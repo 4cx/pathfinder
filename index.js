deb8a5798dc3a3ae06fd1aa1de10ac06ceae4c58
NotEnoughCode <NotEnoughCode@users.noreply.github.com>
2020-03-19 14:19:58 +0100

    automate grid

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 document.write("Hello World!");
+
+function init(length) {
+    var table = document.getElementById("grid");
+    for (x = 0; x < length; x++){
+        var row = table.insertRow(x);
+        for (x = 0; x < length; x++){
+            var cell = row.insertCell(y);
+            cell.innerHTML = String(x) + String(y);
+        }
+    }    
+}
