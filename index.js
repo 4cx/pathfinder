b2e8e992cdd736f5cef792dc9c7ee199c1713158
NotEnoughCode <61588952+NotEnoughCode@users.noreply.github.com>
2020-03-19 22:08:45 +0100

    Added changed colors

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-//document.alert("Hello World!");
+//document.write("Hello World!");
+<<<<<<< HEAD
 
 function changeClass(cell){
-    console.log(12);
+    //console.log(cell.lastChild.data);
+    cell.classList.toggle('wall');
 }
+=======
+>>>>>>> 3894086475af96c4f6652004e4f628c9821b1e74
 
+var table = document.getElementById("grid");
 function init(length) {
-   
+<<<<<<< HEAD
     var table = document.getElementById('grid');
     
+    while(table.hasChildNodes())                    // to remove old table if size is changed
+    {
+        table.removeChild(table.firstChild);
+    }
+    
     for (let x = 0; x < length; x++){
+=======
+    for (x = 0; x < length; x++){
+>>>>>>> 3894086475af96c4f6652004e4f628c9821b1e74
         var row = table.insertRow(x);
         for (let y = 0; y < length; y++){
             var cell = row.insertCell(y);
             cell.innerHTML = String(x) + String(y);
-            cell.addEventListener("click", changeClass(cell)); 
+            //cell.addEventListener("click", changeClass(cell)); 
         }
+<<<<<<< HEAD
     }
+    table.addEventListener("click", function(e) {
+  if (e.target && e.target.nodeName == "TD") {
+    //alert('Cell clicked');
+    changeClass(e.target);
+  }
+});
 }
 
+function submitNum(){
+    var num = document.getElementById('gridLength').value;
+    //alert(num);
+    init(Number(num));
+}
+//init(5);
+=======
+    }    
+}
 init(5);
-document.getElementById('grid').addEventListener("click", changeClass("")); 
+>>>>>>> 3894086475af96c4f6652004e4f628c9821b1e74
